bf8e05e6a2f8ac0e7e888cc843b96d322df17f2f
brotato777 <123862938+brotato777@users.noreply.github.com>
2025-04-30 13:44:04 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號：\"01257015\"\n姓名：\"蘇奕勳\"");
     System.out.println("\"學號：\"01257022\"\n姓名：\"郭浩\"");
+        System.out.println("\"學號：\"01257019\"\n姓名：\"劉俊麟\"");
     }
     }
