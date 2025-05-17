4bf213c691121fb1649bfce907032b2cf0de7690
james su <151006820+jamessu0530@users.noreply.github.com>
2025-05-17 15:31:25 +0800

    Delete FirstClass4Team.java

--- FirstClass4Team.java
+++
@@ -1,8 +0,0 @@
-public class FirstClass4Team {
-    public static void main(String[] args) {
-    System.out.println("這是用來練習GitHub協作的程式!");
-    System.out.println("\"學號：\"01257015\"\n姓名：\"蘇奕勳\"");
-    System.out.println("\"學號：\"01257022\"\n姓名：\"郭浩\"");
-        System.out.println("\"學號：\"01257019\"\n姓名：\"劉俊麟\"");
-    }
-    }
